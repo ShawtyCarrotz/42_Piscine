5ba8599b7b704ec52e004146e85f92c16a781510
Ines Filipa Pais Monteiro <ipais-mo@c1r8s11.42porto.com>
2024-01-29 19:36:03 +0000

    add c_08

---
+++ c_08/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ipais-mo <ipais-mo@student.42porto.co      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/29 17:48:06 by ipais-mo          #+#    #+#             */
+/*   Updated: 2024/01/29 17:52:18 by ipais-mo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+#define FT_H 
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int	ft_strlen(char *str);
+int	ft_strcmp(char *s1, char *s2);
+
+#endif
